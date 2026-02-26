8533dd664f398a6de7b1314e6847935f5f80379c
smarvind817-hue <smarvind817@gmail.com>
2026-02-26 09:50:03 +0530

    UC2:Hardcoded palindrome
    # Conflicts:
    #	src/PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,6 @@
-public class PalindromeCheckerApp{
-
-
-    static void main() {
-
+public class PalindromeCheckerApp {
+    public static void main(String[]args){
+        System.out.println("application name palindrome checker app");
+        System.out.println("version 3.0");
     }
 }
